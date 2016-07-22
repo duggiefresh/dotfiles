cf9caaa7603888b6e4dc18033b1536f7ed756b61
Doug Yun <yun.douglas@gmail.com>
2016-07-21 23:06:57 -0400

    Emacs: zenburn theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,4 +5,16 @@
 (require 'evil)
 (evil-mode 1)
 
+;; Interactively Do Things
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;; Automagically indent
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; Line numbers
 (global-linum-mode 1)
+
+;; Theme
+(load-theme 'zenburn t)
