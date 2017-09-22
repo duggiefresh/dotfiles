b6e810699195cc0428dd8b7e41d1a8b9e38dc3de
duggiefresh <doug.yun@dockyard.com>
2017-09-22 12:06:06 -0700

    Update setting `dired-omit-mode`

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -201,16 +201,12 @@
 
 (add-hook 'dired-load-hook
           (lambda ()
-            (load "dired-x")
+            (require "dired-x")
             ;; Set dired-x global variables here.  For example:
             ;; (setq dired-guess-shell-gnutar "gtar")
             ;; (setq dired-x-hands-off-my-keys nil)
             ))
-(add-hook 'dired-mode-hook
-          (lambda ()
-            ;; Set dired-x buffer-local variables here.  For example:
-              (dired-omit-mode 1)
-            ))
+(setq dired-omit-mode t)
 
 ;;; JS setup
 (setq-default indent-tabs-mode nil)
