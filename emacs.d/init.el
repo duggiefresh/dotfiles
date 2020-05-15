ebe8d25fed38f72152b5b6feae32804b75383bec
duggiefresh <yun.douglas@gmail.com>
2020-05-14 22:50:44 -0700

    Include `.eex` files for webmode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -141,6 +141,7 @@
 (add-to-list 'auto-mode-alist '("\\.scss?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.eex\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
